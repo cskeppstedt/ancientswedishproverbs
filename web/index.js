81e2f75e5dbfab5beba1566d1dc5f14e7dbc4556
Christoffer Skeppstedt <chris.skeppstedt@klarna.com>
2015-06-27 10:44:15 -0400

    Basic Web implementation

--- web/index.js
+++ web/index.js
@@ -1,19 +1,30 @@
 import 'babel/polyfill';
 
 import express  from 'express';
+import * as db  from '../shared/db';
 
 
 /* SETUP */
 var app = express();
 
 app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 
 /* ROUTES */
 app.get('/', (request, response) => {
-    response.send('foo');
+    db.list().then( (posts) => {
+        response.render('index', {
+            posts: posts
+        });
+    }).catch( (err) => {
+        console.log(err);
+        response.render('error', {
+            err: err
+        });
+    });
 });
 
 
